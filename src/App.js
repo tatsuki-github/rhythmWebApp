2cb4d3524eb24086329c972a454a9a431685bf48
tatsuki-github <t-tsuchiyama@ilab.pu-kumamoto.ac.jp>
2022-02-12 21:05:45 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  render() {
+    // 定数nameを定義
+    const hello = "Hello World"
+
+    return (
+      <div>
+        {/* 定数helloを用いて表示 */}
+        <h1>{hello}</h1>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld;
